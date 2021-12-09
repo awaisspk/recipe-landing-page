6c29542f981d998406df6d033fa0738f8e2b3cfc
awaisspk <iamawaisiqball@gmail.com>
2021-12-10 01:18:11 +0500

    Feat: Project layout

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import {Footer} from '@components/Footer';
+import {styled} from '@stitches';
+
+const StyledLayout = styled('main', {
+  width: 'min(900px, 90%)',
+  marginX: 'auto',
+  mt: '30px',
+  minH: '100vh',
+  display: 'grid',
+  gridTemplateRows: '100px auto 40px',
+});
+
+type LayoutProps = {
+  footer?: boolean;
+};
+
+export const Layout: React.FC<LayoutProps> = (props) => {
+  const {footer, children} = props;
+  return (
+    <StyledLayout>
+      {children}
+      {footer && <Footer />}
+    </StyledLayout>
+  );
+};
